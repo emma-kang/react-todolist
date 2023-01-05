8737b363558a1e6a9274d51f8bd620d513789898
Emma Kang <emma140626@gmail.com>
2023-01-04 21:12:32 -0500

    initial commit
    * modified structure
    * add matrerial ui
    * change class component to functional component

---
+++ src/mock/todos.js
@@ -0,0 +1,26 @@
+export const mockTodos = [
+  {
+    "userId": 1,
+    "id": 1,
+    "task": "read books",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "task": "study programming",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "task": "playing game",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "task": "take vitamin",
+    "completed": false
+  }
+]
