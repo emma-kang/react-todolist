072f0dbe80f383620f999740fd121616d54f8f1a
Emma Kang <emma140626@gmail.com>
2023-01-07 20:05:19 -0500

    re-structure folders

--- front/src/components/TodoListItem.js
+++ front/src/components/TodoListItem.js
@@ -3,23 +3,33 @@ import {useState} from "react";
 
 export default function TodoListItem({todos}) {
   const [checked, setChecked] = useState(todos.completed);
+
+  const handleClick = () => {
+    setChecked(!checked);
+  }
   const handleChange = (event) => {
     setChecked(event.target.checked);
-    // following: call api to store each todo current status
+    // following: call api to store each todo's current status
   }
 
   return (
     <ListItem key={todos.id} sx={{width: '100%'}}>
-      <ListItemButton role={undefined} dense>
+      <ListItemButton role={undefined} dense onClick={handleClick}>
         <ListItemIcon>
           <Checkbox
             edge="start"
             checked={checked}
             onChange={handleChange}
             tabIndex={-1}
+            sx={{
+              color: '#bc6c25',
+              '&.Mui-checked': {
+                color: '#bc6c25'
+              }
+            }}
           />
         </ListItemIcon>
-        <ListItemText id={todos.id} primary={todos.task}/>
+        <ListItemText id={todos.id} primary={todos.task} sx={{ textDecorationLine: checked ? 'line-through' : 'none'}}/>
       </ListItemButton>
     </ListItem>
   )
