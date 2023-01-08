22503cd85449120d50c66172b9808985189a8921
Emma Kang <emma140626@gmail.com>
2023-01-07 20:46:24 -0500

    Modified front-end code
    * change page title
    * modified UI
    * add delete button
    * add icon package

--- front/src/components/MainContainer.js
+++ front/src/components/MainContainer.js
@@ -4,7 +4,7 @@ import TodoList from "./TodoList";
 export default function MainContainer() {
   return (
     <>
-      <Typography variant="h2" fontWeight={500} mt={4} mb={4}>To Do List</Typography>
+      <Typography variant="h3" fontWeight={500} mt={4} mb={4}>To Do List</Typography>
       <TodoList />
     </>
   )
--- front/src/components/TodoListItem.js
+++ front/src/components/TodoListItem.js
@@ -1,5 +1,6 @@
-import {Checkbox, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import {Checkbox, IconButton, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
 import {useState} from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function TodoListItem({todos}) {
   const [checked, setChecked] = useState(todos.completed);
@@ -12,25 +13,38 @@ export default function TodoListItem({todos}) {
     // following: call api to store each todo's current status
   }
 
+  const handleDelete = () => {
+    console.log(`delete clicked, current todo id is ${todos.id}`)
+    // following: call api to delete the todo
+  }
+
   return (
-    <ListItem key={todos.id} sx={{width: '100%'}}>
-      <ListItemButton role={undefined} dense onClick={handleClick}>
-        <ListItemIcon>
-          <Checkbox
-            edge="start"
-            checked={checked}
-            onChange={handleChange}
-            tabIndex={-1}
-            sx={{
-              color: '#bc6c25',
-              '&.Mui-checked': {
-                color: '#bc6c25'
-              }
-            }}
-          />
-        </ListItemIcon>
-        <ListItemText id={todos.id} primary={todos.task} sx={{ textDecorationLine: checked ? 'line-through' : 'none'}}/>
-      </ListItemButton>
-    </ListItem>
+    <>
+      <ListItem key={todos.id} sx={{width: '100%'}}
+        secondaryAction={
+        <IconButton edge="end" aria-label="delete" onClick={handleDelete}>
+          <DeleteIcon />
+        </IconButton>
+        }
+      >
+        <ListItemButton role={undefined} dense onClick={handleClick}>
+          <ListItemIcon>
+            <Checkbox
+              edge="start"
+              checked={checked}
+              onChange={handleChange}
+              tabIndex={-1}
+              sx={{
+                color: '#bc6c25',
+                '&.Mui-checked': {
+                  color: '#bc6c25'
+                }
+              }}
+            />
+          </ListItemIcon>
+          <ListItemText id={todos.id} primary={todos.task} sx={{ textDecorationLine: checked ? 'line-through' : 'none'}}/>
+        </ListItemButton>
+      </ListItem>
+    </>
   )
 }
