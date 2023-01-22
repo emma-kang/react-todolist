d5e3b8bd319d9c978b95556b9ed81b0ff9814168
Emma Kang <emma140626@gmail.com>
2023-01-22 11:45:35 -0500

    Add files

---
+++ backend/routes/todo.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+const todoController = require("../controllers/todo.controller");
+const router = express.Router();
+
+// GET All todos
+
+// POST New task
+
+// Update task
+
+// Delete task
+
+module.exports = router;
---
+++ backend/routes/user.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const express = require("express");
+const userController = require("../controllers/user.controller");
+const router = express.Router();
+
+// GET All Users
+
+
+module.exports = router;
